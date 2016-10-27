d090422e6e300b8496dabbc7d1fd8fb1feca1a5c
ninagnedin <ninagnedin@gmail.com>
2016-10-27 08:54:54 -0400

    Add files via upload

--- testFunctions.py
+++ testFunctions.py
@@ -13,7 +13,7 @@ class TestFunctions(unittest.TestCase):
         dx = 1.e-3
         Df_x = F.ApproximateJacobian(f, x0, dx)
         self.assertEqual(Df_x.shape, (1,1))
-        self.assertAlmostEqual(Df_x, slope)
+        self.assertAlmostEqual(float(Df_x[0]), slope) #changed matrix to float
 
     def testApproxJacobian2(self):
         A = N.matrix("1. 2.; 3. 4.")
@@ -24,12 +24,33 @@ class TestFunctions(unittest.TestCase):
         Df_x = F.ApproximateJacobian(f, x0, dx)
         self.assertEqual(Df_x.shape, (2,2))
         N.testing.assert_array_almost_equal(Df_x, A)
+        
+    def testApproxJacobian3(self):
+        A = N.matrix("1. 2. 3.; 4. 5. 6.; 7. 8. 9.")
+        def f(x):
+            return A * x
+        x0 = N.matrix("10; 11; 12")
+        dx = 1.e-6
+        Df_x = F.ApproximateJacobian(f, x0, dx)
+        self.assertEqual(Df_x.shape, (3,3))
+        N.testing.assert_array_almost_equal(Df_x, A)
 
     def testPolynomial(self):
         # p(x) = x^2 + 2x + 3
         p = F.Polynomial([1, 2, 3])
         for x in N.linspace(-2,2,11):
             self.assertEqual(p(x), x**2 + 2*x + 3)
+    
+    def testAnalyticJacobian1(self):
+        def f(x):
+            return N.matrix([ [3.*x[1,0]] ,[x[0,0]] ])
+        x0 = N.matrix([[2.], [1.]])
+        Df_x = F.AnalyticJacobian(f, x0)
+        N.testing.assert_array_almost_equal(Df_x, N.matrix([[3.],[2.]]))
+        
+
+        
+    
 
 if __name__ == '__main__':
     unittest.main()
