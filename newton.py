3e9366ca9238654ed2bf0ae98e7a71176367f909
ninagnedin <ninagnedin@gmail.com>
2016-10-27 08:53:58 -0400

    Add files via upload

--- newton.py
+++ newton.py
@@ -7,32 +7,54 @@ import numpy as N
 import functions as F
 
 class Newton(object):
-    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6):
+    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6,Df=None,r=None):
         """Return a new object to find roots of f(x) = 0 using Newton's method.
         tol:     tolerance for iteration (iterate until |f(x)| < tol)
         maxiter: maximum number of iterations to perform
-        dx:      step size for computing approximate Jacobian"""
+        dx:      step size for computing approximate Jacobian
+        Df:      analytical expression for Jacobian
+        r:       approximated root must lie within radius r of initial guess x0
+        y0:      for function of 2 parameters although solver will only solve along x dimension"""
         self._f = f
         self._tol = tol
         self._maxiter = maxiter
         self._dx = dx
+        if(r is None):
+        #Default value for r - since there's no alternative need a default
+            self._r=20 
+        else:
+            self._r=r
+        #If it's not entered it will remain none and will use approx later
+        self._Df=Df 
 
-    def solve(self, x0):
+    def solve(self, x0,y0=None):
         """Return a root of f(x) = 0, using Newton's method, starting from
         initial guess x0"""
         x = x0
         for i in xrange(self._maxiter):
-            fx = self._f(x)
+            if y0 is not None:
+                fx=self._f(x,y0)
+            else:
+                fx = self._f(x)
             if N.linalg.norm(fx) < self._tol:
                 return x
-            x = self.step(x, fx)
+            x = self.step(x, fx,y0) 
+            if N.linalg.norm(x-x0)>self._r:
+                raise Exception("Root not within threshold radius of initial guess")
+        #Check for convergence and raises exception if it does not converge
+        if N.linalg.norm(fx)>self._tol:
+            raise Exception("Did not converge in provided number of iterations")
         return x
 
-    def step(self, x, fx=None):
+    def step(self, x, fx=None,y0=None):
         """Take a single step of a Newton method, starting from x
         If the argument fx is provided, assumes fx = f(x)"""
         if fx is None:
             fx = self._f(x)
-        Df_x = F.ApproximateJacobian(self._f, x, self._dx)
+        if self._Df is None:
+            #If y0 is passed as a none it will get caught in if statement later
+            Df_x = F.ApproximateJacobian(self._f, x, self._dx,y0)
+        else:
+            Df_x=F.AnalyticJacobian(self._Df,x)
         h = N.linalg.solve(N.matrix(Df_x), N.matrix(fx))
-        return x + h
+        return x - float(h[0]) #Chaged to float(h[0]) to avoid adding matrix to float, switched to -
